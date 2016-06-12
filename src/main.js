fb5554b934ad0abf9722766821d139d95cf747f7
yintothayang <yintothayang@gmail.com>
2016-06-11 17:45:17 -0700

    remove export

--- src/main.js
+++ src/main.js
@@ -1,5 +1,4 @@
 export {EntityManager as em, default as default} from './EntityManager.js'
 export {default as BaseEntity} from './BaseEntity.js'
 export {default as BaseSystem} from './BaseSystem.js'
-export {default as BaseEvent} from './BaseEvent.js'
 
