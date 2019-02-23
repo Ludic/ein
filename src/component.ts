897ce44502e846a1b75a551bfe6f43a4862bfef3
yin <yintothayang@gmail.com>
2019-02-23 15:54:28 -0800

    ComponentType tests

--- src/component.ts
+++ src/component.ts
@@ -1,10 +1,15 @@
 import { Entity } from './entity'
-
+// type Klass<T> = { new (...args: any[]): T }
 /**
  * Interface for all Components. A Component is intended as a data holder and provides data to be processed in an
  * [[System]]. But do as you wish.
  * [[System]]s have no state and [[Component]]s have no behavior!!!!!
  */
+
+interface Klass<T> {
+  new(): T
+}
+
 export class Component {}
 
 /**
@@ -20,4 +25,86 @@ export class ComponentManager {
   public remove(entity: Entity): void {
 		entity.notifyComponentRemoved()
   }
+}
+
+
+/**
+ * Uniquely identifies a {@link Component} sub-class. It assigns them an index which is used internally for fast comparison and
+ * retrieval. See {@link Family} and {@link Entity}. ComponentType is a package protected class. You cannot instantiate a
+ * ComponentType. They can only be accessed via {@link #getIndexFor(Class<? extends Component>)}. Each component class will always
+ * return the same instance of ComponentType.
+ * @author Stefan Bachmann
+ */
+export class ComponentType {
+  // componentInstance.constructor -> ComponentType
+  private static classMap = new WeakMap
+  // private static ObjectMap<Class<? extends Component>, ComponentType> assignedComponentTypes = new ObjectMap<Class<? extends Component>, ComponentType>();
+  private static typeIndex: number = 0
+	private index: number
+
+	constructor(){
+		this.index = ComponentType.typeIndex++
+	}
+
+	/** @return This ComponentType's unique index */
+	public getIndex(): number {
+		return this.index
+	}
+
+	/**
+	 * @param componentType The {@link Component} class
+	 * @return A ComponentType matching the Component Class
+	 */
+	public static getFor<T>(componentConstructor: Klass<T>): ComponentType {
+		let componentType: ComponentType = ComponentType.classMap.get(componentConstructor)
+
+		if(!componentType){
+			componentType = new ComponentType()
+      ComponentType.classMap.set(componentConstructor, componentType)
+		}
+
+		return componentType
+	}
+
+	/**
+	 * Quick helper method. The same could be done via {@link ComponentType.getFor(Class<? extends Component>)}.
+	 * @param componentType The {@link Component} class
+	 * @return The index for the specified {@link Component} Class
+	 */
+	public static getIndexFor<T>(componentConstructor: Klass<T>): number {
+		return this.getFor(componentConstructor).getIndex()
+	}
+
+	/**
+	 * @param componentTypes list of {@link Component} classes
+	 * @return Bits representing the collection of components for quick comparison and matching. See
+	 *         {@link Family#getFor(Bits, Bits, Bits)}.
+	 */
+
+  // TODO return Bits()
+	public static getBitsFor<T extends Component>(componentConstructors: Array<Klass<T>>): number[] {
+    return []
+
+	  // Bits bits = new Bits();
+
+		// int typesLength = componentTypes.length;
+		// for (int i = 0; i < typesLength; i++) {
+		// 	bits.set(ComponentType.getIndexFor(componentTypes[i]));
+		// }
+
+		// return bits;
+	}
+
+
+	public hashCode(): number {
+		return this.index
+	}
+
+	public equals(obj: Object): boolean {
+		if(this == obj) return true
+		if(obj == null) return false
+		if(this.constructor.name != obj.constructor.name) return false
+		let other: ComponentType = obj as ComponentType
+		return this.index == other.index;
+	}
 }
---
+++ tests/components.spec.ts
@@ -0,0 +1,36 @@
+import { assert } from 'chai'
+
+import { Component, ComponentType } from '@lib/component'
+import MovementComponent from '@tests/components/movement'
+
+class ComponentA implements Component {}
+class ComponentB implements Component {}
+
+describe('Components', () => {
+
+  it('should have a valid ComponentType', async () => {
+    assert.isNotNull(ComponentType.getFor(ComponentA))
+    assert.isNotNull(ComponentType.getFor(ComponentB))
+  })
+
+  it('should have the same ComponentType', async () => {
+    const componentType1: ComponentType = ComponentType.getFor(ComponentA)
+	  const componentType2: ComponentType = ComponentType.getFor(ComponentA)
+	  assert.equal(true, componentType1.equals(componentType2))
+	  assert.equal(true, componentType2.equals(componentType1))
+	  assert.equal(componentType1.getIndex(), componentType2.getIndex())
+	  assert.equal(componentType1.getIndex(), ComponentType.getIndexFor(ComponentA))
+    assert.equal(componentType2.getIndex(), ComponentType.getIndexFor(ComponentA))
+  })
+
+  it('should have different ComponentType(s)', async () => {
+    const componentType1: ComponentType = ComponentType.getFor(ComponentA)
+	  const componentType2: ComponentType = ComponentType.getFor(ComponentB)
+	  assert.equal(false, componentType1.equals(componentType2))
+	  assert.equal(false, componentType2.equals(componentType1))
+	  assert.notEqual(componentType1.getIndex(), componentType2.getIndex())
+	  assert.notEqual(componentType1.getIndex(), ComponentType.getIndexFor(ComponentB))
+    assert.notEqual(componentType2.getIndex(), ComponentType.getIndexFor(ComponentA))
+  })
+
+})
