c8e284f328af7dc7d16e89a20228c4a2f79cc178
yin <yintothayang@gmail.com>
2019-02-24 22:51:35 -0800

    all entities tests passing

--- tests/entities.spec.ts
+++ tests/entities.spec.ts
@@ -20,68 +20,67 @@ interface Klass<T> {
   new(): T
 }
 
-
 const am: ComponentMapper<ComponentA> = ComponentMapper.getFor(ComponentA.prototype as Klass<ComponentA>)
 const bm: ComponentMapper<ComponentB> = ComponentMapper.getFor(ComponentB.prototype as Klass<ComponentB>)
 
 describe('Entities', () => {
 
-  // it('should addAndReturnComponent()', async () => {
-  //   const entity: Entity = new Entity()
-  //   const componentA: ComponentA = new ComponentA()
-  //   const componentB: ComponentB = new ComponentB()
+  it('should addAndReturnComponent()', async () => {
+    const entity: Entity = new Entity()
+    const componentA: ComponentA = new ComponentA()
+    const componentB: ComponentB = new ComponentB()
 
-  //   assert.equal(componentA, entity.addAndReturn(componentA))
-  //   assert.equal(componentB, entity.addAndReturn(componentB))
+    assert.equal(componentA, entity.addAndReturn(componentA))
+    assert.equal(componentB, entity.addAndReturn(componentB))
 
-  //   assert.equal(2, entity.getComponents().length)
-  // })
+    assert.equal(2, entity.getComponents().length)
+  })
 
-  // it('should check for no Components', async () => {
-  //   const entity: Entity = new Entity()
+  it('should check for no Components', async () => {
+    const entity: Entity = new Entity()
 
-  //   // TODO
-  //   // assert.isTrue(entity.getComponentBits().isEmpty())
-  //   assert.equal(0, entity.getComponents().length)
-  //   assert.isNull(am.get(entity))
-  //   assert.isNull(bm.get(entity))
-  //   assert.isFalse(am.has(entity))
-  //   assert.isFalse(bm.has(entity))
-  // })
+    // TODO
+    // assert.isTrue(entity.getComponentBits().isEmpty())
+    assert.equal(0, entity.getComponents().length)
+    assert.isNull(am.get(entity))
+    assert.isNull(bm.get(entity))
+    assert.isFalse(am.has(entity))
+    assert.isFalse(bm.has(entity))
+  })
 
-  // it('should add() and remove() Components', async () => {
-  //   const entity: Entity = new Entity()
-  //   entity.add(new ComponentA())
+  it('should add() and remove() Components', async () => {
+    const entity: Entity = new Entity()
+    entity.add(new ComponentA())
 
-  //   assert.equal(1, entity.getComponents().length)
+    assert.equal(1, entity.getComponents().length)
 
-  //   // TODO
-  //   // Bits componentBits = entity.getComponentBits()
-  //   // const componentAIndex: number = ComponentType.getIndexFor(ComponentA)
+    // TODO
+    // Bits componentBits = entity.getComponentBits()
+    // const componentAIndex: number = ComponentType.getIndexFor(ComponentA)
 
-  //   // TODO
-  //   // for (int i = 0; i < componentBits.length(); ++i) {
-  //   // 	assert.equal(i == componentAIndex, componentBits.get(i));
-  //   // }
+    // TODO
+    // for (int i = 0; i < componentBits.length(); ++i) {
+    // 	assert.equal(i == componentAIndex, componentBits.get(i));
+    // }
 
-  //   assert.isNotNull(am.get(entity))
-  //   assert.isNull(bm.get(entity))
-  //   assert.isTrue(am.has(entity))
-  //   assert.isFalse(bm.has(entity))
+    assert.isNotNull(am.get(entity))
+    assert.isNull(bm.get(entity))
+    assert.isTrue(am.has(entity))
+    assert.isFalse(bm.has(entity))
 
-  //   entity.remove(ComponentA.prototype as Klass<ComponentA>)
-  //   assert.equal(0, entity.getComponents().length)
+    entity.remove(ComponentA.prototype as Klass<ComponentA>)
+    assert.equal(0, entity.getComponents().length)
 
-  //   // TODO
-  //   // for (int i = 0; i < componentBits.length(); ++i) {
-  //   // 	assertFalse(componentBits.get(i));
-  //   // }
+    // TODO
+    // for (int i = 0; i < componentBits.length(); ++i) {
+    // 	assertFalse(componentBits.get(i));
+    // }
 
-  //   assert.isNull(am.get(entity))
-  //   assert.isNull(bm.get(entity))
-  //   assert.isFalse(am.has(entity))
-  //   assert.isFalse(bm.has(entity))
-  // })
+    assert.isNull(am.get(entity))
+    assert.isNull(bm.get(entity))
+    assert.isFalse(am.has(entity))
+    assert.isFalse(bm.has(entity))
+  })
 
   it('should add() and remove() all Components', async () => {
     const entity: Entity = new Entity()
@@ -120,72 +119,62 @@ describe('Entities', () => {
 
   })
 
+  it('should add the same Component', async () => {
+    const entity: Entity = new Entity()
+
+    const a1: ComponentA = new ComponentA()
+    const a2: ComponentA = new ComponentA()
+
+    entity.add(a1)
+    entity.add(a2)
+
+    assert.equal(1, entity.getComponents().length)
+    assert.isTrue(am.has(entity))
+    assert.notEqual(a1, am.get(entity))
+    assert.equal(a2, am.get(entity))
+  })
+
+  it('should have working componentListeners', async () => {
+    const addedListener: EntityListenerMock = new EntityListenerMock()
+    const removedListener: EntityListenerMock = new EntityListenerMock()
+
+    const entity: Entity = new Entity()
+    entity.componentAdded.add(addedListener)
+    entity.componentRemoved.add(removedListener)
+
+    assert.equal(0, addedListener.counter)
+    assert.equal(0, removedListener.counter)
+
+    entity.add(new ComponentA())
+    assert.equal(1, addedListener.counter)
+    assert.equal(0, removedListener.counter)
+
+    entity.remove(ComponentA.prototype as Klass<ComponentA>)
+    assert.equal(1, addedListener.counter)
+    assert.equal(1, removedListener.counter)
+
+    entity.add(new ComponentB())
+    assert.equal(2, addedListener.counter)
+
+    entity.remove(ComponentB.prototype as Klass<ComponentB>)
+    assert.equal(2, removedListener.counter)
+  })
+
+  it('should getComponentByClass', async () => {
+    const compA: ComponentA = new ComponentA()
+    const compB: ComponentB = new ComponentB()
+
+    const entity: Entity = new Entity()
+    entity.add(compA).add(compB)
+
+    const retA: ComponentA | null = entity.getComponentForClass(ComponentA.prototype as Klass<ComponentA>)
+    const retB: ComponentB | null = entity.getComponentForClass(ComponentB.prototype as Klass<ComponentB>)
+
+    assert.isNotNull(retA)
+    assert.isNotNull(retB)
+
+    assert.isTrue(retA == compA)
+    assert.isTrue(retB == compB)
+  })
+
 })
-
-
-
-
-// @Test
-// public void addSameComponent () {
-// 	Entity entity = new Entity();
-
-// 	ComponentA a1 = new ComponentA();
-// 	ComponentA a2 = new ComponentA();
-
-// 	entity.add(a1);
-// 	entity.add(a2);
-
-// 	assert.equal(1, entity.getComponents().size());
-// 	assert.isTrue(am.has(entity));
-// 	assertNotEquals(a1, am.get(entity));
-// 	assert.equal(a2, am.get(entity));
-// }
-
-// @Test
-// public void componentListener () {
-// 	EntityListenerMock addedListener = new EntityListenerMock();
-// 	EntityListenerMock removedListener = new EntityListenerMock();
-
-// 	Entity entity = new Entity();
-// 	entity.componentAdded.add(addedListener);
-// 	entity.componentRemoved.add(removedListener);
-
-// 	assert.equal(0, addedListener.counter);
-// 	assert.equal(0, removedListener.counter);
-
-// 	entity.add(new ComponentA());
-
-// 	assert.equal(1, addedListener.counter);
-// 	assert.equal(0, removedListener.counter);
-
-// 	entity.remove(ComponentA.class);
-
-// 	assert.equal(1, addedListener.counter);
-// 	assert.equal(1, removedListener.counter);
-
-// 	entity.add(new ComponentB());
-
-// 	assert.equal(2, addedListener.counter);
-
-// 	entity.remove(ComponentB.class);
-
-// 	assert.equal(2, removedListener.counter);
-// }
-
-// @Test
-// public void getComponentByClass () {
-// 	ComponentA compA = new ComponentA();
-// 	ComponentB compB = new ComponentB();
-
-// 	Entity entity = new Entity();
-// 	entity.add(compA).add(compB);
-
-// 	ComponentA retA = entity.getComponent(ComponentA.class);
-// 	ComponentB retB = entity.getComponent(ComponentB.class);
-
-// 	assert.isNotNull(retA);
-// 	assert.isNotNull(retB);
-
-// 	assert.isTrue(retA == compA);
-// 	assert.isTrue(retB == compB);
-// }
