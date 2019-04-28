9969e5a967883437c982a877ebfb6f854a07f5f4
yin <yintothayang@gmail.com>
2019-04-28 13:31:33 -0700

    update main exports

---
+++ src/Ein.ts
@@ -0,0 +1,32 @@
+import * as Component from './Component'
+import * as ComponentType from './ComponentType'
+import * as ComponentMapper from './ComponentMapper'
+import * as Family from './Family'
+import * as Entity from './Entity'
+import * as EntityListener from './EntityListener'
+import * as System from './System'
+import * as SystemListener from './SystemListener'
+import * as Engine from './Engine'
+
+
+export {Component}
+export {ComponentType}
+export {ComponentMapper}
+export {Family}
+export {Entity}
+export {EntityListener}
+export {System}
+export {SystemListener}
+export {Engine}
+
+export default {
+  Component,
+  ComponentType,
+  ComponentMapper,
+  Family,
+  Entity,
+  EntityListener,
+  System,
+  SystemListener,
+  Engine
+}
