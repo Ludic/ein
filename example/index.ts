e5ee4e92b08f3b9d7debe857378e61eb99388e66
yin <yintothayang@gmail.com>
2020-02-11 20:44:46 +0900

    auto decode/encode transferables

--- example/index.ts
+++ example/index.ts
@@ -1,1 +1,17 @@
 import { Component } from "../src/Component"
+
+let data: any = {
+  x: 0,
+  y: 0
+}
+
+let t = new Component(data)
+
+console.log(t)
+
+t.data = {
+  x: 2,
+  y: 3
+}
+
+console.log(t)
--- src/Component.ts
+++ src/Component.ts
@@ -1,11 +1,16 @@
 // [[file:~/repos/mine/README.org::*Component][Component:1]]
+const encoder = new TextEncoder()
+const decoder = new TextDecoder()
 export class Component {
   _data: any
+  _transferable_data: any
   modified: boolean
+  isTransferable: boolean
 
-  constructor(data: any){
+  constructor(data?: any, isTransferable: boolean = false){
     this.data = data
     this.modified = false
+    this.isTransferable = isTransferable
   }
 
   get data(){
@@ -13,6 +18,17 @@ export class Component {
   }
 
   set data(data){
+    this._data = data
+    this.modified = true
+  }
+
+  get transferable_data(){
+    const view = encoder.encode('taco')
+    console.log(view); // Uint8Array(3)
+    return this._transferable
+  }
+
+  set transferable_data(data){
     this._data = data
     this.modified = true
   }
