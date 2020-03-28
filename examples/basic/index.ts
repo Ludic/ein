d069eb7d5553285542afdc1284eb9d04da49e998
yin <yintothayang@gmail.com>
2020-03-28 15:14:01 +0900

    basic example

--- examples/basic/index.ts
+++ examples/basic/index.ts
@@ -9,7 +9,7 @@ const circle_query: Query = {
 class MovementSystem extends System {
   entities: Entity[]
   entity_query: Query
-
+  angle: number
   constructor(priority: number = 0, enabled: boolean = true) {
     super(priority, enabled)
     this.entity_query = circle_query
@@ -21,7 +21,12 @@ class MovementSystem extends System {
   }
 
   execute(delta: number, time: number): void {
-    console.log("entities: ", this.entities)
+    this.entities.forEach((entity: Entity)=>{
+      let pos: PositionComponent = entity.getComponent(PositionComponent)!
+      pos.data.x = 150 - Math.cos((Math.PI / 180) * pos.data.angle) * 100
+      pos.data.y = 150 + Math.sin((Math.PI / 180) * pos.data.angle) * 100
+      pos.data.angle -= 2
+    })
   }
 }
 
@@ -43,29 +48,38 @@ class RenderSystem extends System {
   }
 
   execute(delta: number, time: number): void {
-    this.ctx.beginPath();
-    this.ctx.arc(75, 75, 10, 0, Math.PI * 2, true)
-    this.ctx.fill()
+    this.ctx.clearRect(0, 0, this.ctx.canvas.width, this.ctx.canvas.height);
+    this.ctx.fillStyle = "steelblue"
+    this.entities.forEach((entity: Entity)=>{
+      const pos: PositionComponent = entity.getComponent(PositionComponent)!
+      this.ctx.save()
+      this.ctx.beginPath();
+      this.ctx.arc(pos.data.x, pos.data.y, 10, 0, Math.PI * 2, true)
+      this.ctx.fill()
+      this.ctx.restore()
+    })
   }
 }
 
 class PositionComponent extends Component {}
-
-
 
 const engine: Engine = new Engine()
 engine.addSystem(MovementSystem)
 engine.addSystem(RenderSystem)
 engine
   .createEntity("circle")
-  .addComponent(PositionComponent, {x: 50, y: 50})
+  .addComponent(PositionComponent, {x: 50, y: 50, angle: 0})
+
+engine
+  .createEntity("circle")
+  .addComponent(PositionComponent, {x: 50, y: 50, angle: 180})
 
 
 let start: any = null
 function update(timestamp: number){
   if(!start) start = timestamp
   engine.execute(timestamp-start, timestamp)
-  // requestAnimationFrame(update)
+  requestAnimationFrame(update)
 }
 
 requestAnimationFrame(update)
