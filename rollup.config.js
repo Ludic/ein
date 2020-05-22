d9118fc70db6b1a7494f6dcbed39186a4d234146
Derek Arner <doman412@gmail.com>
2020-05-21 21:26:41 -0700

    remove unused stuff from rollup config; change module value in package to es build

--- rollup.config.js
+++ rollup.config.js
@@ -1,27 +1,6 @@
 import typescript from 'rollup-plugin-typescript2'
 import resolve from '@rollup/plugin-node-resolve'
 
-import vite from './vite-plugin'
-
-import path from 'path'
-
-const serve = process.env.npm_config_serve == 'true'
-
-const viteConfig = {
-  port: 8080,
-  root: 'examples',
-  resolvers: [
-    {
-      requestToFile(publicPath, root){
-        if(publicPath.includes('/dist')){
-          return path.join(__dirname, publicPath)
-        }
-        return undefined
-      },
-      fileToRequest(){},
-    }
-  ],
-}
 
 export default {
   input: 'src/index.ts',
@@ -50,8 +29,5 @@ export default {
   plugins: [
     typescript(),
     resolve(),
-    ...(serve
-      ? [ vite(viteConfig) ]
-      : []),
   ]
 };
